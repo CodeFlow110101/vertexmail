303f5a69eabdea46f47ba320e97a184a9c76c4eb
codeflow <codeflow110101@gmail.com>
2025-05-02 21:58:06 +0530

    Testing Mail seen

--- resources/views/mails/lead-mail.blade.php
+++ resources/views/mails/lead-mail.blade.php
@@ -1,4 +1,5 @@
 <div>
     {!! $template !!}
-    <img src="{{ route('mail.tracker', ['id' => $id]) }}" width="1" height="1" style="display:block;" alt="">
+    <img src="{{ route('email.open', ['id' => $id]) }}" width="1" height="1" alt="" style="display: none;">
+    <!-- <img src="{{ route('email.open', ['id' => $id]) }}" width="100" height="100" alt="Tracking Pixel"> -->
 </div>
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Response;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/mail-tracker/{id}.png', function ($id) {
-    // Log or store the view
-    Log::info("Email opened for ID: {$id}");
-
-    // Optionally: update DB to mark it as opened
-
+Route::get('/email/open/{id}', function ($id) {
+    // Log or update DB that the email with ID $id was opened
+    // \App\Models\EmailLog::where('id', $id)->update(['opened_at' => now()]);
+    Log::info('hello');
     // Return a 1x1 transparent PNG
-    return response()
-        ->file(public_path('tracker.png'), [
-            'Content-Type' => 'image/png',
-        ]);
-})->name('mail.tracker');
+    $pixel = base64_decode(
+        'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAASsJTYQAAAAASUVORK5CYII='
+    );
+
+    return Response::make($pixel, 200, [
+        'Content-Type' => 'image/png',
+        'Content-Length' => strlen($pixel),
+        'Cache-Control' => 'no-cache, no-store, must-revalidate',
+        'Pragma' => 'no-cache',
+        'Expires' => '0',
+    ]);
+})->name('email.open');
